92d60f407faebe456e89a7786dc50698da380620
MitsuruFujiwara <fujiwara52jp@gmail.com>
2016-04-03 22:21:43 +0900

    修正test
    Putの式間違ってたの修正しといた。あと諸々インスタンス変数化してみた

--- test_BS のコピー.py
+++ test_BS のコピー.py
@@ -2,24 +2,37 @@
 
 # test for github
 #よくわかんない
+
+from scipy.stats import norm
 import numpy as np
 
 class test:
-    def __init__(self, opt_type):
+    def __init__(self, S, K, r, v, T, opt_type):
+        self.S = S
+        self.K = K
+        self.r = r
+        self.v = v
+        self.T = T
         self.opttype = opt_type
         
-    def BS(self,S, K, r, v, T):
-        d1 = (np.log(S/K) + (r+ (v*v*0.5))*T) / (v*np.sqrt(T))
-        d2 = d1 - v*np.sqrt(T)
+    def BS(self):
+        S = self.S
+        K = self.K
+        r = self.r
+        v = self.v
+        T = self.T
+        opttype = self.opttype
+        
+        d1 = (np.log(S/K) + (r + (v*v*0.5))*T) / (v * np.sqrt(T))
+        d2 = d1 - v * np.sqrt(T)
 
-        if self.opttype == "Call":
-            C = S * np.random.randn.cdf(d1) - K*np.random.randn.cdf(d2)*np.exp(-r*T)
+        if opttype == "Call":
+            C = S * norm.cdf(d1) - K * norm.cdf(d2) * np.exp(-r * T)
             return C
-        elif self.opttype =="Put":
-            P = S * np.random.randn.cdf(d1) - K*np.random.randn.cdf(d2)*np.exp(-r*T)
+        elif opttype =="Put":
+            P = K * norm.cdf(-d2) * np.exp(-r * T) - S * norm.cdf(-d1)
             return P
-            
 
 if __name__ == "__main__":
-    t = test("Call")
-    print t.BS(100, 100, 0.01, 0.4, 1)
+    t = test(100, 100, 0.01, 0.4, 1, "Put")
+    print t.BS()
